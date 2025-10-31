e3b211a267bad60ba89f5bcde6ad8a0af3fdf07d
atressler5 <atressler5@gmail.com>
2025-10-30 22:43:23 -0400

    made phase 2 folder

---
+++ Project 1/Phase 2/java/RR.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class RR implements Algorithm {
+
+
+
+    private List<Task> taskArray;
+
+    public RR(List<Task> tasks){
+        taskArray = tasks;
+    }
+
+    public void schedule() {
+
+    }
+
+    public Task pickNextTask() {
+        return null;
+    }
+
+}
