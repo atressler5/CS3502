51aa48b84a77b2211fa7f0239c3c7a18db73b0ba
atressler5 <atressler5@gmail.com>
2025-10-31 01:48:21 -0400

    implemented RR and prio

--- Project 1/Phase 2/java/Priority.java
+++ Project 1/Phase 2/java/Priority.java
@@ -9,13 +9,41 @@ public class Priority implements Algorithm {
         taskArray = tasks;
     }
 
-    public void schedule(){
+    public void schedule() {
+        int timer = 0;
+        while(taskArray.size() > 0){
+            int timeAtStart = timer;
+            Task thisTask = pickNextTask();
+            //amount of time to run
+            int runFor = thisTask.getBurst();
+            //run for entire burst
+            CPU.run(thisTask, runFor);
+            timer += runFor;
+            //System.out.println("Ran for " + runFor);
+            //have it set up like this for the second phase where i will have to reduce by a certain quantum or preempt
+            thisTask.setBurst(thisTask.getBurst() - runFor);
+            if(thisTask.getBurst() == 0) System.out.println("Task " + thisTask.getName() + " finished.");
+            //works if all tasks arrive at time 0
+            System.out.println("Waiting time: " + timeAtStart);
+            System.out.println("Turnaround time: " + timer);
 
+            System.out.print("\n\n");
+
+        }
     }
 
-
-    public Task pickNextTask(){
-        return null;
+    public Task pickNextTask() {
+        //assuming that lines in lower indices arrived earlier
+        if(taskArray.size() == 0) return null;
+        Task max = taskArray.get(0);
+        for(int i = 0; i < taskArray.size(); i++){
+            //if same priority, choose one that arrived earlier
+            if(max.getPriority() < taskArray.get(i).getPriority()){
+                max = taskArray.get(i);
+            }
+        }
+        taskArray.remove(max);
+        return max;
     }
     
 }
--- Project 1/Phase 2/java/RR.java
+++ Project 1/Phase 2/java/RR.java
@@ -1,21 +1,62 @@
-import java.util.List;
+import java.util.*;
 
 public class RR implements Algorithm {
 
-
-
     private List<Task> taskArray;
+    private HashMap<Task, Integer> startTimes;
+    private HashMap<Task, Integer> endTimes;
+    private int quantum;
+    private int timer = 0;
 
-    public RR(List<Task> tasks){
+    public RR(List<Task> tasks) {
         taskArray = tasks;
+        startTimes = new HashMap<Task, Integer>();
+        endTimes = new HashMap<Task, Integer>();
+        // defined in requirements
+        quantum = 10;
     }
 
     public void schedule() {
+        while (taskArray.size() > 0) {
+            Task thisTask = pickNextTask();
+            if(!startTimes.containsKey(thisTask)) startTimes.put(thisTask, timer);
 
+            // amount of time to run
+            int runFor = quantum;
+            // so burst doesn't go negative
+            if (runFor > thisTask.getBurst()) {
+                runFor = thisTask.getBurst();
+            }
+
+            CPU.run(thisTask, runFor);
+            timer += runFor;
+            // System.out.println("Ran for " + runFor);
+            // have it set up like this for the second phase where i will have to reduce by
+            // a certain quantum or preempt
+            thisTask.setBurst(thisTask.getBurst() - runFor);
+
+            if (thisTask.getBurst() == 0) {
+                endTimes.put(thisTask, timer);
+                System.out.println("Task " + thisTask.getName() + " finished.");
+                // works if all tasks arrive at time 0
+                System.out.println("Waiting time: " + startTimes.get(thisTask));
+                System.out.println("Turnaround time: " + endTimes.get(thisTask));
+            }
+            //requeue if not complete
+            else taskArray.add(thisTask);
+
+            System.out.print("\n\n");
+
+        }
     }
 
     public Task pickNextTask() {
-        return null;
+        // assuming that lines in lower indices arrived earlier
+        if (taskArray.size() == 0)
+            return null;
+        Task next = taskArray.get(0);
+        taskArray.remove(0);
+        return next;
     }
 
 }
